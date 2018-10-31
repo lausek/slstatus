7a0198e570948fa05c39b8914e2e774fd69c1f77
lausek <root@lausek.eu>
2018-10-31 19:27:23 +0100

    added experimental visual charging state

--- config.def.h
+++ config.def.h
@@ -62,5 +62,5 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ datetime, "%s",           "%F %T" },
-	{ batter_perc_vis, NULL, NULL },
+	{ battery_perc_vis, NULL, NULL },
 };
